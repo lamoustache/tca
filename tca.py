0e6a445a752c61060e97e8f2efbdb529e782d714
moustache <lamoustache@gmail.com>
2013-01-22 14:40:37 +0000

    initial commit

---
+++ tca.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+import sys
+import requests
+
+from scapy.all import *
+
+# TCP Connection to the directory authorities
+
+def tcp_connection_check(directory_authorities):
+    for directory in directory_authorities:
+        host = directory.split(":",1)
+        ans, unans = sr(IP(dst=host[0])/TCP(dport=int(host[1]),flags="S"), verbose=0)
+        ans.summary(lfilter = lambda (s,r): r.sprintf("%TCP.flags%") == "SA",
+        prn=lambda(s,r):r.sprintf("TCP connection successful to %IP.src%:%TCP.sport% "))
+
+
+# Download consensus from directory authorities
+
+def dl_consensus(directory_authorities):
+    for directory in directory_authorities:
+        r = requests.get("http://" + directory + "/tor/status-vote/current/consensus.z")
+        if r.status_code == requests.codes.ok:
+            with open("consensus_"+directory+".z", "wb") as file:
+                file.write(r.content)
+            print "[*] OK Consensus downloaded"
+            
+# Connect to Tor public relay
+
+def tor_connection():
+    pass
+
+    
+directory_authorities = [ '128.31.0.39:9131', '86.59.21.38:80',
+                          '194.109.206.212:80', '76.73.17.194:9030',
+                          '212.112.245.170:80', '193.23.244.244:80',
+                          '208.83.223.34:443', '171.25.193.9:443',
+                          '154.35.32.5:80' ]
+                          
+bridges_torproject_org = ['bridges.torproject.org:443']
+
+tcp_connection_check(directory_authorities)
+
+tcp_connection_check(bridges_torproject_org)
+
+#dl_consensus(directory_authorities)
